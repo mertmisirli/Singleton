6b849542190a982e60c1cf7b61703d6ba2010d0c
Furkan Mert Mısırlı <78733884+mertmisirli@users.noreply.github.com>
2023-11-20 23:22:30 +0300

    Add project files.

---
+++ StaticConstructor/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+class MyClass
+{
+    static MyClass()
+    {
+        Console.WriteLine("Static Constructor");
+    }
+
+    public MyClass()
+    {
+        Console.WriteLine("Instance Created");
+    }
+}
